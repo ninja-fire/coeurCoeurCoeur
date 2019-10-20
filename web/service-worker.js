48b6305e2f3cda068f16e745217eec43b8f51394
jules goullée <julesgoullee@gmail.com>
2019-10-20 19:11:46 +0200

    pwa

--- web/service-worker.js
+++ web/service-worker.js
@@ -1,5 +1,27 @@
 /* eslint-env serviceworker */
 import 'babel-polyfill';
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+self.workbox.setConfig({
+  debug: true
+});
+
+self.workbox.loadModule('workbox-strategies');
+
+const { strategies } = self.workbox;
+
+const fileTypes = [ 'html', 'js', 'css', 'jpg', 'png', 'gif', 'svg', 'eot', 'ttf', 'woff', 'woff2'];
+
+self.addEventListener('fetch', (event) => {
+
+  if(fileTypes.find(fileType => event.request.url.endsWith(fileType) ) ){
+
+    const cacheFirst = new strategies.CacheFirst();
+    event.respondWith(cacheFirst.makeRequest({request: event.request}));
+
+  }
+
+});
 
 self.addEventListener('push', async (event) => {
 
